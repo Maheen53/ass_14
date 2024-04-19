272fdf3def30f238458518febf984ef1fa806ca4
“[firstname <Maheen53@users.noreply.github.com>
2024-04-19 16:30:40 -0700

    ass_14

---
+++ main.js
@@ -0,0 +1,13 @@
+let guestlist = ["anoosha", "aiman", "narmeen", "asma"];
+// guestlist.forEach(list => {
+//     console.log(`hi how are you, ${guestlist} you are invited to iftar party at pc hotel 
+//     regards by Maheen Imtiaz "MI" `)
+// })
+// console.log(`hi how are you, ${guestlist [0]} you are invited to iftar party at pc hotel 
+// //     regards by Maheen Imtiaz "MI" `)
+// console.log(`hi how are you, ${guestlist [1]} you are invited to iftar party at pc hotel 
+// //     regards by Maheen Imtiaz "MI" `)
+guestlist.forEach(guest => {
+    console.log(`dear ${guest} you are cordialy invited a dinner`);
+});
+export {};
